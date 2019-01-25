8d70b63c8967308d4ad815514bb6e06dee12e892
John Hayes <johnnyhayes7@gmail.com>
2019-01-25 17:03:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  name_array.each do |name|
+    puts "Hello, my name is #{name}"
+  end
+end
