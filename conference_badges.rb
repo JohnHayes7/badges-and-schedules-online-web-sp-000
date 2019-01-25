e3b1eabfbb98a8c449390b97a16654b3dc16a792
John Hayes <johnnyhayes7@gmail.com>
2019-01-25 17:04:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,8 +4,3 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(name_array)
-  name_array.each do |name|
-    puts "Hello, my name is #{name}"
-  end
-end
